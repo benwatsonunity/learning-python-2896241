7be23225fec554c506d7c2a5dc81e68381329dda
Ben Watson <ben.watson@unity3d.com>
2023-06-04 18:39:09 +0100

    working on exercise solution

---
+++ Ch2 - Basics/challenge_start.py
@@ -0,0 +1,33 @@
+answer = input ("Enter string to test for palindrome or 'exit':")
+
+print(answer)
+print(answer[::-1])
+
+def reverse_answer(answer):
+    result = answer[::-1]
+    return result
+
+print(reverse_answer(answer))
+
+if answer == (reverse_answer(answer)):
+    print("palindrome")
+
+# def reverse(answer):
+#     result = ""
+#     for i in range(len(text),0,-1):
+#         result += text[i-1]
+#     return (result) 
+#     print(result)  
+
+# palindrome = 
+
+# if answer = 
+
+# user_input_reversed = list(reversed(answer))
+# print(user_input_reversed)
+
+# value = user_input_reversed
+# if user_input_reversed == answer:
+#     result = "palindrome detected"
+# print(result)
+
